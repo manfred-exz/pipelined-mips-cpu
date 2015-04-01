578649f6be43894249c5720ffafc10b8fb71173b
manfred-exz <manfred.exz@gmail.com>
2015-04-01 15:46:52 +0800

    top added
    merge this pipelined cpu with previous soc with multi-cycle cpu

--- pipeif.v
+++ pipeif.v
@@ -2,10 +2,11 @@ module pipeif(pc_source, pc, branch_pc, register_pc, jump_pc, next_pc, pc_plus_4
 	input	[31:0]	pc, branch_pc, register_pc, jump_pc;
 	input	[1:0]	pc_source;
 
-	output	[31:0]	next_pc, pc_plus_4, inst;
+	// output	[31:0]	next_pc, pc_plus_4, inst;
+	output	[31:0]	next_pc, pc_plus_4;
+	input 	[31:0]	inst;
 
 	mux4x32 update_next_pc(pc_plus_4, branch_pc, register_pc, jump_pc, pc_source, next_pc);
 	cla32 pc_plus4(pc, 32'h4, 1'b0, pc_plus_4);
-	pipeimem inst_mem(pc, inst);
 
 endmodule
--- pipemem.v
+++ pipemem.v
@@ -22,3 +22,28 @@ module pipemem(we, addr, datain, clk, inclk, outclk, dataout);
 //	defparam ram.lpm_address_control	= " registered";
 
 endmodule
+
+//module pipemem(we, addr, datain, clk, inclk, outclk, dataout);
+//	
+//	input 	[31:0]	addr, datain;
+//	input			clk, we, inclk, outclk;
+//
+//	output	[31:0]	dataout;
+//
+//	wire	write_enable = we & ~clk;
+//					
+//	ip_ram ram( .clka(inclk),
+//				.wea(write_enable),
+//				.addra(addr[6:2]),
+//				.dina(datain),
+//				.douta(dataout)
+//				);
+//				
+////	defparam ram.lpm_width				= 32;
+////	defparam ram.lpm_widthad			= 5;
+////	defparam ram.lpm_indata				= "registered";
+////	defparam ram.lpm_outdata			= "registered";
+////	defparam ram.lpm_file				= "pipedmem.mif";
+////	defparam ram.lpm_address_control	= " registered";
+//
+//endmodule
